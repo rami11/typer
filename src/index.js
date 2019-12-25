545679c9fcc07632f8883c7a0abfa29ad80b3bec
Rami Serapian <rsn@Ramis-MacBook-Pro.local>
2019-12-25 02:07:04 -0500

    Generate random text on page refresh

--- src/index.js
+++ src/index.js
@@ -2,15 +2,26 @@ import Typer from "./typer/Typer";
 
 class Main {
   constructor() {
+    this.TEXTS = [
+      "This is text no 1.",
+      "Here is another text",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eleifend sem risus, ut faucibus magna elementum ut. Quisque sit amet fermentum tellus. Aliquam aliquet sed eros sed mattis. Donec consequat velit ac hendrerit pulvinar. Nam et risus ligula. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse et sapien nisi. Curabitur vitae velit a dui consectetur mattis sit amet at dui. Nullam ac euismod diam. Nam congue nec arcu sed fermentum. Nunc consequat nisi varius, dignissim libero ac, viverra massa."
+    ];
+
     // Theme
     let app = document.querySelector("body");
     app.classList.add("app");
 
-    new Typer("test app.");
+    let text = this._generateText();
+    console.log("text", text);
+
+    new Typer(text);
   }
 
   _generateText() {
-    return "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eleifend sem risus, ut faucibus magna elementum ut. Quisque sit amet fermentum tellus. Aliquam aliquet sed eros sed mattis. Donec consequat velit ac hendrerit pulvinar. Nam et risus ligula. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse et sapien nisi. Curabitur vitae velit a dui consectetur mattis sit amet at dui. Nullam ac euismod diam. Nam congue nec arcu sed fermentum. Nunc consequat nisi varius, dignissim libero ac, viverra massa.";
+    let i = Math.floor(Math.random() * Math.floor(this.TEXTS.length));
+    console.log(i);
+    return this.TEXTS[i];
   }
 }
 
