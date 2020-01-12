349ba33d856535e99f54a1d7ca193ce0d18b4455
Rami Serapian <rsn@Ramis-MacBook-Pro.local>
2020-01-12 13:57:54 -0500

    Refactoring

--- dist/typer.sql
+++ dist/typer.sql
@@ -26,7 +26,7 @@ CREATE TABLE `texts_en` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `text` varchar(75) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -35,7 +35,7 @@ CREATE TABLE `texts_en` (
 
 LOCK TABLES `texts_en` WRITE;
 /*!40000 ALTER TABLE `texts_en` DISABLE KEYS */;
-INSERT INTO `texts_en` VALUES (4,'It’s not about ideas. It’s about making ideas happen.'),(5,'Always deliver more than expected'),(6,'The most courageous act is still to think for yourself. Aloud'),(7,'What would you do if you were not afraid?'),(8,'Nothing will work unless you do.'),(9,'Surround yourself with only people who are going to lift you higher.'),(10,'Sweating the details is more important than anything else.'),(11,'Your time is limited, so don’t waste it living someone else’s life.');
+INSERT INTO `texts_en` VALUES (13,'Hi there');
 /*!40000 ALTER TABLE `texts_en` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -59,7 +59,7 @@ CREATE TABLE `texts_fr` (
 
 LOCK TABLES `texts_fr` WRITE;
 /*!40000 ALTER TABLE `texts_fr` DISABLE KEYS */;
-INSERT INTO `texts_fr` VALUES (2,'Dans une grande âme tout est grand.'),(3,'Chacun voit midi à sa porte.'),(4,'Je pense, donc je suis.'),(5,'On ne change pas une équipe qui gagne.'),(6,'Prouver que j’ai raison serait accorder que je puis avoir tort.'),(7,'On n’est point toujours une bête pour l’avoir été quelquefois.'),(8,'Il n’y a pas de verités moyennes.'),(9,'Chassez le naturel, il revient au galop.'),(10,'Il vaut mieux prévenir que guérir.'),(11,'Le temps est un grand maître, dit-on. Le malheur est qui’il tue ses élèves.'),(12,'Être adulte, c’est être seul.'),(13,'Un homme seul est toujours en mauvaise compagnie.'),(14,'A vaincre sans peril, on triomphe sans gloire.'),(15,'L’enfer, c’est les autres.'),(16,'Autres temps, autres mœurs.'),(17,'La vérité vaut bien qu’on passe quelques années sans la trouver.');
+INSERT INTO `texts_fr` VALUES (2,'Dans une grande âme tout est grand.'),(3,'Chacun voit midi à sa porte.'),(4,'Je pense, donc je suis.'),(5,'On ne change pas une équipe qui gagne.'),(9,'Chassez le naturel, il revient au galop.'),(10,'Il vaut mieux prévenir que guérir.'),(13,'Un homme seul est toujours en mauvaise compagnie.'),(14,'A vaincre sans peril, on triomphe sans gloire.');
 /*!40000 ALTER TABLE `texts_fr` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -72,4 +72,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-01-04 19:48:27
+-- Dump completed on 2020-01-12 13:42:55
